26349e1fd8649f1366066bc2f78d392587f3c238
randymized <ot40ddj02@sneakemail.com>
2012-01-27 19:47:30 -0600

    Use req.malifi.utils to get hostname

--- test/fixtures/common/sites.js
+++ test/fixtures/common/sites.js
@@ -3,8 +3,7 @@ var map= {
   localhost: site
  ,'127.0.0.1': site
 }
-extractHostFromHost= /([^:]+).*/
 
 module.exports= function(req) {
-  return map[req.headers.host.replace(extractHostFromHost,'$1')]
+  return map[req.malifi.utils.hostname()]
 }
