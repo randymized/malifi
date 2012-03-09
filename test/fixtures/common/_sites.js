e3c4841a8552ea180ccfd9c241b6a3bf7382f200
randymized <ot40ddj02@sneakemail.com>
2012-03-09 17:42:23 -0600

    Add sample.com site.  Rerouting allows testing site routing, so here's a site.

--- test/fixtures/common/_sites.js
+++ test/fixtures/common/_sites.js
@@ -1,12 +1,14 @@
 var background= __dirname+'/../background';
+var sample= __dirname+'/../sample.com';
 var map= {
   localhost: background
  ,'127.0.0.1': background
+ ,'sample.com': sample
 }
 
 exports= module.exports= {
   lookup: function() {
       return map[this.host.name]
     }
-  ,paths: [background]
+  ,paths: [background,sample]
 }
